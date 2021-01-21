77f4f22ec01eebbd027c02bad8ddb85cf72ee542
Larry Gamboa <techmedia.lg@gmail.com>
2021-01-20 20:46:42 -0800

    add functionality to pop up form

--- app.js
+++ app.js
@@ -1,36 +1,106 @@
+// const nodemailer = require("nodemailer");
+
+// // async..await is not allowed in global scope, must use a wrapper
+// async function main() {
+//   // Generate test SMTP service account from ethereal.email
+//   // Only needed if you don't have a real mail account for testing
+//   let testAccount = await nodemailer.createTestAccount();
+
+//   // create reusable transporter object using the default SMTP transport
+//   let transporter = nodemailer.createTransport({
+//     host: "smtp.ethereal.email",
+//     port: 587,
+//     auth: {
+//       user: "zane.ratke30@ethereal.email",
+//       pass: "H7Qh5b2Ab77JwVWfcx",
+//     },
+//   });
+
+//   // send mail with defined transport object
+//   let info = await transporter.sendMail({
+//     from: '"Zane Ratke" <zane.ratke30@ethereal.email>', // sender address
+//     to: "feelthehousegroove@yahoo.com", // list of receivers
+//     subject: "Hello, this is a test email", // Subject line
+//     text: "Hello world. This is a test!!", // plain text body
+//     html: "<b>Hello world. This is a test!!</b>", // html body
+//   });
+
+//   console.log("Message sent: %s", info.messageId);
+//   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+//   // Preview only available when sending through an Ethereal account
+//   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+//   // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+// }
+
+// main().catch(console.error);
+
+
+const express = require("express");
+const cors = require("cors");
 const nodemailer = require("nodemailer");
+const multiparty = require("multiparty");
+require("dotenv").config();
 
-// async..await is not allowed in global scope, must use a wrapper
-async function main() {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  let testAccount = await nodemailer.createTestAccount();
+const PORT = process.env.PORT || 5000;
 
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    auth: {
-      user: "zane.ratke30@ethereal.email",
-      pass: "H7Qh5b2Ab77JwVWfcx",
-    },
+// instantiate an express app
+const app = express();
+// cors
+app.use(cors({ origin: "*" }));
+
+app.use("/public", express.static(process.cwd() + "/public")); //make public static
+
+const transporter = nodemailer.createTransport({
+  service: "hotmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+});
+
+// verify connection configuration
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
+});
+
+app.post("/send", (req, res) => {
+  let form = new multiparty.Form();
+  let data = {};
+  form.parse(req, function (err, fields) {
+    console.log(fields);
+    Object.keys(fields).forEach(function (property) {
+      data[property] = fields[property].toString();
+    });
+    console.log(data);
+    const mail = {
+      sender: `${data.name} <${data.email}>`,
+      to: process.env.EMAIL, // receiver email,
+      subject: data.subject,
+      text: `${data.name} <${data.email}> \n${data.message}`,
+    };
+    transporter.sendMail(mail, (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Something went wrong.");
+      } else {
+        res.status(200).send("Email successfully sent to recipient!");
+      }
+    });
   });
+});
 
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"Zane Ratke" <zane.ratke30@ethereal.email>', // sender address
-    to: "feelthehousegroove@yahoo.com", // list of receivers
-    subject: "Hello, this is a test email", // Subject line
-    text: "Hello world. This is a test!!", // plain text body
-    html: "<b>Hello world. This is a test!!</b>", // html body
-  });
+//Index page (static HTML)
+app.route("/").get(function (req, res) {
+  res.sendFile(process.cwd() + "/public/index.html");
+});
 
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-}
-
-main().catch(console.error);
+/*************************************************/
+// Express server listening...
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
